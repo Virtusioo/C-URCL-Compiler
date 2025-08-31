a5142207a55d2f60a4f2cf17d80bb421e0d96387
dave virtusio <virtusiodavidrafael7.snfss@gmail.com>
2025-08-31 08:21:59 +0800

    Initial Setup

---
+++ src/lexer.hpp
@@ -0,0 +1,26 @@
+
+#pragma once
+
+enum class TokenType
+{
+    IDENT,
+    STRING,
+
+    UNSIGNED,
+    INT,
+    FLOAT,
+    CHAR,
+    LONG,
+    DOUBLE,
+
+    STAR,
+    SLASH,
+    PLUS,
+    MINUS,
+    PERCENT,
+
+    ELLIPSIS,
+    POINT,
+    
+
+};
